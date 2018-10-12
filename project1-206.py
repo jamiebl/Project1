dceba9ed108f9002c4fd9e5559e76f813b88cbb3
Jamie Blackwell <jamiebl@umich.edu>
2018-10-12 11:57:07 -0400

    final commit
    Fixed any bugs/problems
    Did extra credit part as well this time
    ^0^

--- project1-206.py
+++ project1-206.py
@@ -11,23 +11,39 @@ def getData(file):
 #Ouput: return a list of dictionary objects where
 #the keys are from the first row in the data. and the values are each of the other rows
 	#pdb.set_trace()
+	"""This function takes in a file from your computer/files. It will return a
+
+	list of dictionaries. The keys are from the first row in the data provided,
+	with the values being the following rows. See comments on specific dets on how
+	the function runs.
+	"""
+	#Open the file
 	in_file = open(file, "r")
+
+	#Note: First line == header. Use next() to start reading from the second line
 	next(in_file)
-	line = in_file.readline() #grab the first line
-	list_of_dict = [] # an empty list to append everything together at the end
+
+	#Grabs the first line read, which is the second line in the file...
+	line = in_file.readline()
+
+	#Emp list to append the dictionaries
+	list_of_dict = []
+
 
 	while line:
-		dic_file = {} #a dictionary for the file objects
+		#Create dictionary, might also be able to do this outside the loop...
+		dic_file = {}
+
 		value = line.split(",")
 
-		#establish values
+		#Establish/Make values
 		first_name = value[0]
 		last_name = value[1]
 		email = value[2]
 		class_year = value[3]
 		date_of_birh = value[4]
 
-		#set up dictionary w/ keys and value_city_state
+		#Set up dictionary w/ keys and values
 		dic_file["First"] = first_name
 		dic_file["Last"] = last_name
 		dic_file["Email"] = email
@@ -37,6 +53,7 @@ def getData(file):
 		list_of_dict.append(dic_file)
 		line = in_file.readline()
 
+	#Don't forget to close file at the end when using a while loop.
 	in_file.close()
 	return list_of_dict
 
@@ -46,12 +63,22 @@ def mySort(data,col):
 # Sort based on key/column
 #Input: list of dictionaries and col (key) to sort on
 #Output: Return the first item in the sorted list as a string of just: firstName lastName
+	"""This function takes in a list of dictionaries and a key to sort on. This
 
-	#First sort list based on key
+	function will return the first item/student in that sorted list as a string.
+	See comments for more specific dets.
+	"""
+
+	#Sort list based on key, but not a specific key
 	sort_list = sorted(data, key = lambda val: val[col])
-	grab_first_item = sort_list[0] #grab first line/item/dictionary in list
+
+	#Grab the first item(index 0) in that sorted list
+	grab_first_item = sort_list[0]
+
+	#Grab the "first name" and "last name" key values from previous item
 	grab_first_n = str(grab_first_item["First"])
 	grab_first_l = str(grab_first_item["Last"])
+
 
 	return grab_first_n + " " + grab_first_l
 	pass
@@ -63,27 +90,39 @@ def classSizes(data):
 # Output: Return a list of tuples sorted by the number of students in that class in
 # descending order
 # [('Senior', 26), ('Junior', 25), ('Freshman', 21), ('Sophomore', 18)]
+	"""This function takes in a list of dictionaries and returns a list of tuples
 
-	list_tuple = [] # Create a empty list for tuples
+	sorted by the number of students in that class from highest --> lowest.
+	"""
+
+	#Create an emp list for the tuples
+	list_tuple = []
+
+	#Estab values
 	fresh_count = 0
 	sopho_count = 0
 	junior_count = 0
 	senior_count = 0
 
+	#Sort students by Class key from highest to low.
 	list_stu = sorted(data, key = lambda col: col["Class"], reverse = True)
 
+	#List comprehension instead of for loop, and then take the number of stu in each class
+	#Probably could have shortened this into one longer loop, but oh well
 	senior_count = len([val for val in list_stu if val["Class"] == 'Senior'])
 	jun_count = len([val for val in list_stu if val["Class"] == 'Junior'])
-	so_count = len([val for val in list_stu if val["Class"] == 'Sophomore'])
-	f_count = len([val for val in list_stu if val["Class"] == 'Freshman'])
+	soph_count = len([val for val in list_stu if val["Class"] == 'Sophomore'])
+	fresh_count = len([val for val in list_stu if val["Class"] == 'Freshman'])
 
 
 	list_tuple.append(("Senior", senior_count))
 	list_tuple.append(("Junior", jun_count))
-	list_tuple.append(("Sophomore", so_count))
-	list_tuple.append(("Freshman", f_count))
+	list_tuple.append(("Sophomore", soph_count))
+	list_tuple.append(("Freshman", fresh_count))
 
+	#Sort list of the four classes by the second column(value) from high-->low
 	sort_list = sorted(list_tuple, key = lambda col: col[1], reverse = True)
+
 	return (sort_list)
 
 	pass
@@ -93,19 +132,26 @@ def findMonth(a):
 # Find the most common birth month form this data
 # Input: list of dictionaries
 # Output: Return the month (1-12) that had the most births in the data
+	"""This function takes in a list of dictionaries and returns the month (1-12)
 
-	#Want just the DOB keys-successful
+	that had the most births in the data. See comments for more spec details.
+	"""
+
+	#Made a deepycopy so OG data would not have been harmed/broken in any way
 	dif = copy.deepcopy(a)
+
+	#Want only the DOB keys-successful
 	for value in dif:
 		del value["Class"]
 		del value["Email"]
 		del value["First"]
 		del value["Last"]
 
-	#Now I have a list of the values from DOB
+	#Now I have a list of the values from the DOB key
+	#Probably could have dif too...eh
 	data_dob = [val["DOB"] for val in dif]
-	#del data_dob[0]
 
+	#Emp list for the month specfic data
 	data_mo  = []
 
 	#List of months/days/years
@@ -113,15 +159,16 @@ def findMonth(a):
 	for str in data_dob:
 		value = str.split('/')
 		month = int(value[0])
-		day =   int(value[1])
+		day  =  int(value[1])
 		year = int(value[2])
 
 		data_mo.append(month)
 
 	#make emp dict in order to hold the key(num) and val(freq)
 	number_counter = {}
+
 	for number in data_mo:
-		#if that number is currently in the dict, then do not Standard
+		#if that number is currently in the dict, then do not increment
 		#else increment before moving on to the next number
 		if number in number_counter:
 			number_counter[number] += 1
@@ -131,9 +178,7 @@ def findMonth(a):
 	#sort using dict get function/ for values with the highest freq at the top
 	num = sorted(number_counter, key = number_counter.get, reverse = True)
 
-	#Just return the first
-	#print (num)
-	#print (number_counter)
+	#Just return the first num in list
 	top_1 = num[0]
 
 	return top_1
@@ -146,24 +191,23 @@ def mySortPrint(a,col,fileName):
 # as first,last,email
 #Input: list of dictionaries, col (key) to sort by and output file name
 #Output: No return value, but the file is written
+	"""This function takes in a list of dictionaries and returns nothing. However, a
 
+	file is written in csv format.
+	"""
 	#Sort list by keys
 	sort_list = sorted(a, key = lambda val : val[col])
 
+	#Open file
 	f = open(fileName, "w")
 
-	#b = [val for val in sort_list if val["First"] == "First" != sort_list]
-	#print(b)
-
+	#Run for loop where adding first name, last name, email to a file
 	for value in sort_list:
 		first = value["First"]
 		last = value["Last"]
 		email = value["Email"]
 		f.write(first + "," + last + "," + email + "\n")
-
-		#print(first + "," + last + "," + email)
 	f.close()
-	#print (a)
 
 
 
@@ -176,6 +220,55 @@ def findAge(a):
 # Output: Return the average age of the students and round that age to the nearest
 # integer.  You will need to work with the DOB and the current date to find the current
 # age in years.
+	"""This function takes in a list of dictionaries and returns the average sum_age
+
+	of the students. The number returned is rounded to the nearest integer.
+	See comments below for specific details on how to do/what happens in the func.
+	"""
+
+	#Create a deepcopy so the OG data is not messed up accidently
+	dif = copy.deepcopy(a)
+	#Run a for loop in order to delete the keys not needed
+	for value in dif:
+		del value["Class"]
+		del value["Email"]
+		del value["First"]
+		del value["Last"]
+
+	#Now I have a list of the values from  the DOB key ONLY
+	data_dob = [val["DOB"] for val in dif]
+
+	#Initialize an emp list for for a list of the years
+	data_year  = []
+
+	#List of months/days/years
+	#Split at the "/" in order to get three categories
+	for str in data_dob:
+		value = str.split('/')
+		month = int(value[0])
+		day =   int(value[1])
+		year = int(value[2])
+
+		data_year.append(year)
+
+	#Grab the current Year using datetime, 'date' will work, but is a diff format
+	now = datetime.now()
+
+	#Initialize an emp list for a list of current_age
+	current_age = []
+
+	#Run that for loop in order to get the current agesself.
+	#Note: abs() is needed for the years that are greater than the current year
+	for value in data_year:
+		age = abs(now.year - value)
+		current_age.append(age)
+
+	#Find average: sum/amount of numbers in list = aver
+	sum_age = sum(current_age)
+	amount = len(current_age)
+
+	return(int(sum_age/round(amount)))
+
 
 	pass
 
